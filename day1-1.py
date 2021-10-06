99b870801a67ad4216ceb62f5d83ca76bf7c8a18
AshIsbitt <ashisbitt@icloud.com>
2021-10-06 13:05:15 +0100

    First challenge complete

---
+++ day1-1.py
@@ -0,0 +1,27 @@
+# Find the two entries that sum up to 2020; what do you get if you multiply them together?
+
+def findNumsEqualToSumAndReturnProduct(filename, sumValue):
+	# Import the file with numbers
+	with open(filename, 'r') as inputFile:
+		numberList = inputFile.readlines()
+
+	print(f"Searching for {sumValue}")
+	for numbers in range(len(numberList)):
+		currentFirstNum = int(numberList[numbers])
+		secondNum = 0
+
+		print(f"Iterating through {currentFirstNum}")
+
+		for iterable in range(len(numberList)):
+			# print(f"{numberList[iterable]}")
+			if (int(numberList[iterable]) + currentFirstNum) == sumValue:
+				secondNum = int(numberList[iterable])
+				print("*****FOUND IT*****")
+				break
+
+		if secondNum != 0:
+			break
+
+	return f"{currentFirstNum=}, {secondNum=}, Total={currentFirstNum*secondNum}"
+
+print(findNumsEqualToSumAndReturnProduct("SuppliedInputs/day1-1.txt", 2020))
