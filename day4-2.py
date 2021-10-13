286a6dc45bf92c8bc6db3d9be7ae8a1831e4d057
AshIsbitt <ashisbitt@icloud.com>
2021-10-13 13:38:37 +0100

    Day 4 pt2 incomplete

---
+++ day4-2.py
@@ -0,0 +1,91 @@
+# In your batch file, how many passports are valid.
+
+import re
+
+def validatePassport(passport):
+	hexaPattern = re.compile(r'#[0-9a-fA-F]')
+
+	if len(passport) != 8 and not (len(passport) == 7 and 'cid' not in passport):
+		print(f'1: {len(passport)}')
+
+		if len(passport) == 7:
+			print(passport.keys())
+
+		return False
+	elif int(passport['byr']) not in range(1920, 2003):
+		print(f"2: {passport['byr']}")
+		return False
+	elif int(passport['iyr']) not in range(2010, 2021):
+		print(f"3: {passport['iyr']}")
+		return False
+	elif int(passport['eyr']) not in range(2020, 2031):
+		print(f"4: {passport['eyr']}")
+		return False
+	elif passport['hgt'][-2:] not in ['cm', 'in']:
+		print(f"5: {passport['hgt']}")
+		return False
+	elif passport['hgt'][-2:] == "cm":
+		if int(passport['hgt'][:-2]) not in range(150, 194):
+			print(6)
+			return False
+	elif passport['hgt'][-2:] == "in":
+		if int(passport['hgt'][:-2]) not in range(59, 77):
+			print(f"7: {passport['hgt']}")
+			return False
+	elif len(passport['hcl'] != 7) and not re.search(hexaPattern, passport['hcl']):
+		print(f"8: {passport['hcl']}")
+		return False
+	elif passport['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+		print(f"9: {passport['ecl']}")
+		return False
+	elif len(passport['pid']) != 9 or not passport['pid'].isDigit():
+		print(10)
+		return False
+
+	return True
+
+
+def passportCheck(filename):
+	with open(filename, 'r') as InputFile:
+		batchData = InputFile.readlines()
+
+	passports = []
+	passport = {}
+	validPassportsCount = 0
+	tempLine = ""
+
+	for line in batchData:
+		# Split non-empty lines to individual dicts
+		if line != '\n':
+			tempLine += str(line)
+
+		if line == '\n':
+			tempLine = tempLine.replace('\n', ' ')
+			tempfield = tempLine.rstrip().split(' ')
+			tempfield = [field.split(':') for field in tempfield]
+			tempLine = ''
+
+			for field in tempfield:
+				passport[field[0]] = field[1]
+
+		# Store passport dict into passports list
+		passports.append(passport)
+		passport = {}
+
+	# Check each passport dict for length based on valid criteria
+	for passport in passports:
+		if len(passport) == 0:
+			passports.remove(passport)
+			continue
+
+		validPassport = validatePassport(passport)
+		print(f'{validPassport=}')
+		if validPassport:
+			validPassportsCount += 1
+		elif validPassport == None:
+			print(f'NONE: {passport}')
+
+	return f'{validPassportsCount=}'
+
+
+print(passportCheck('SuppliedInputs/day4.txt'))
