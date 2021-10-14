21f06bbdba90f0bac1403a318746691e8a1e6b84
AshIsbitt <ashisbitt@icloud.com>
2021-10-14 15:47:20 +0100

    Day 5 part 2

---
+++ day5-2.py
@@ -0,0 +1,45 @@
+# What is the highest seat ID on a boarding plane?
+# Binary tree searching
+
+# Convert the input into binary number
+def seatParser(line):
+	binaryString = ''
+
+	for char in line:
+		match char:
+			case 'F':
+				binaryString += '0'
+			case 'B':
+				binaryString += '1'
+			case 'L':
+				binaryString += '0'
+			case 'R':
+				binaryString += '1'
+
+	binaryNum = int(binaryString, 2)
+
+	return binaryNum
+
+
+def mySeat(seatArray):
+	empty = 0
+
+	for potentialSeat in range(0, len(seatArray)+1):
+		if (potentialSeat not in seatArray) and (potentialSeat + 1 in seatArray) and (
+		potentialSeat - 1 in seatArray):
+			return potentialSeat
+
+def main(filename):
+	with open(filename, 'r') as fileInput:
+		batchData = fileInput.readlines()
+
+	seatIDArray = []
+
+	for line in batchData:
+		seatIDArray.append(seatParser(line))
+
+	mySeatID = mySeat(seatIDArray)
+	return mySeatID
+
+if __name__ == '__main__':
+	print(main('SuppliedInputs/day5.txt'))
