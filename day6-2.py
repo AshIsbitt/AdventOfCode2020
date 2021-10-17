4491fa73295a17f3821bd586c672fca7f0e886f6
AshIsbitt <ashisbitt@icloud.com>
2021-10-17 12:55:49 +0100

    day 6 part 2 using string defaults

---
+++ day6-2.py
@@ -0,0 +1,33 @@
+# For each group, count the number of questions to which everyone answered 'yes'. What is the sum
+# of those counts
+
+import string
+
+def countAnswers(customsData):
+	customsData = customsData.rstrip()
+	peopleInGroup = customsData.count('\n') + 1
+	groupYesAnswers = ''
+
+	customsData = customsData.replace('\n', '')
+
+	for letter in list(string.ascii_lowercase):
+		if customsData.count(letter) == peopleInGroup:
+			groupYesAnswers += letter
+
+	return len(groupYesAnswers)
+
+
+def main(filename):
+	with open(filename, 'r') as inputFile:
+		customsFormData = inputFile.read().split('\n\n')
+
+	sumOfCounts = 0
+
+	for item in customsFormData:
+		sumOfCounts += countAnswers(item)
+
+	print(f'{sumOfCounts=}')
+
+
+if __name__ == '__main__':
+	raise SystemExit(main('SuppliedInputs/day6.txt'))
