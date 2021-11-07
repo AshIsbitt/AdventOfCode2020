63d6033a766908fea6e28dde56554996bbdbc157
AshIsbitt <ashisbitt@icloud.com>
2021-11-07 13:04:11 +0000

    Day 8 pt 1 complete with failing mypy checks

--- day8.py
+++ day8.py
@@ -2,33 +2,55 @@
 # the accumulator
 
 
+def bufChecker(
+    bufHistory: list[int], instruction: list[int, str]
+) -> tuple[bool, list[int]]:
+    instructionPointer = instruction[0]
+
+    if instructionPointer in bufHistory:
+        return True, bufHistory
+    else:
+        return False, bufHistory
+
+
+def interpreter(instructionSet: list[str]) -> int:
+    accumulator: int = 0
+    bufHistory: list[int] = []
+    pointer: int = 0
+
+    instructionsWithPointers = tuple(enumerate(instructionSet))
+
+    while True:
+        instruction = list(instructionsWithPointers[pointer])
+        instruction[1] = instruction[1].rstrip()
+
+        print(
+            f"Pointer: {pointer},",
+            f"instruction: {instruction},",
+            f"accumulator: {accumulator}",
+        )
+
+        alreadyExecuted, bufHistory = bufChecker(bufHistory, instruction)
+
+        print(f"{alreadyExecuted=}")
+        if alreadyExecuted == True:
+            return accumulator
+        else:
+            bufHistory.append(instruction[0])
+
+        if instruction[1][:3] == "jmp":
+            pointer += int(instruction[1][4:])
+        else:
+            pointer += 1
+            if instruction[1][:3] == "acc":
+                accumulator += int(instruction[1][4:])
+
+
 def main(filename: str) -> int:
     with open(filename) as rawInstructions:
         instructionSet = rawInstructions.readlines()
 
-    accumulator: int = 0
-    instructionHistory: set[int] = set()
-    pointer: int = 0
-
-    while True:
-        print(f"Index: {pointer}, instruction: {instructionSet[pointer]}")
-
-        if pointer in instructionHistory:
-            break
-        else:
-            # This is wrong - need to think about getting pointer as the index
-            # of the list and manipulating that to get the individual
-            # instructions
-            instructionHistory = {*(pointer)}
-
-        if instruction[:3] == "jmp":
-            index += int(instruction[4:])
-        elif instruction[:3] == "acc":
-            accumulator += int(instruction[4:])
-        elif instruction[:3] == "nop":
-            continue
-
-        pointer
+    accumulator = interpreter(instructionSet)
 
     print(f"{accumulator=}")
     return 0
