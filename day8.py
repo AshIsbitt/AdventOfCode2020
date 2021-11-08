ca1a25348979252486500ac4afb0e58be3725e1c
AshIsbitt <ashisbitt@icloud.com>
2021-11-08 17:44:05 +0000

    Day 8 pt 2 almost works, stuck in an infinite loop and type hinting not complete

--- day8.py
+++ day8.py
@@ -16,7 +16,33 @@ def bufChecker(
         return False, bufHistory
 
 
-def interpreter(instructionSet: list[str]) -> int:
+def checkInterpretation(numOfInstructionsRun: int, instructionSet: list[str]) -> bool:
+    if numOfInstructionsRun != len(instructionSet):
+        return False
+    else:
+        return True
+
+
+def attemptedRepair(instructionSet: list[str], changedIndex: str) -> list[str]:
+    # starting at list index changedIndex, I want to find the next nop/jmp
+    # and flip it, then change changedIndex to show how many further forward
+    # the function has checked. I also want to skip JMP with a positive value
+    # as they should not be able to cause an infinite loop
+
+    for index, instruction in enumerate(instructionSet[changedIndex:]):
+        if instruction[:3] == "jmp":  # and instruction[4] == '-':
+            instruction.replace("jmp", "nop")
+            changedIndex = index
+            break
+        elif instruction[:3] == "nop":
+            instruction.replace("nop", "jmp")
+            changedIndex = index
+            break
+
+    return instructionSet, changedIndex
+
+
+def interpreter(instructionSet: list[str]) -> tuple[int, int]:
     accumulator: int = 0
     bufHistory: list[int] = []
     pointer: int = 0
@@ -28,21 +54,23 @@ def interpreter(instructionSet: list[str]) -> int:
     while True:
         instruction: tuple[int, str] = instructionsWithPointers[pointer]
 
-        print(
+        """print(
             f"Pointer: {pointer},",
             f"instruction: {instruction},",
             f"accumulator: {accumulator}",
-        )
+        )"""
 
         alreadyExecuted, bufHistory = bufChecker(bufHistory, instruction)
 
         if alreadyExecuted == True:
-            return accumulator
+            return accumulator, len(bufHistory)
         else:
             bufHistory.append(instruction[0])
 
         if instruction[1][:3] == "jmp":
             pointer += int(instruction[1][4:])
+            if pointer < 0:
+                pointer == 0
         else:
             pointer += 1
             if instruction[1][:3] == "acc":
@@ -53,7 +81,22 @@ def main(filename: str) -> int:
     with open(filename) as rawInstructions:
         instructionSet = rawInstructions.readlines()
 
-    accumulator = interpreter(instructionSet)
+    changedIndex: int = 0
+    hasTerminatedSuccessfully: bool = False
+
+    while not hasTerminatedSuccessfully:
+        print(f"Testing instruction set with index:{changedIndex=} changed")
+        instructionSet, changedIndex = attemptedRepair(instructionSet, changedIndex)
+        changedIndex += 1
+        print(f"{changedIndex=}")
+
+        accumulator, numOfInstructionsRun = interpreter(instructionSet)
+        hasTerminatedSuccessfully: bool = checkInterpretation(
+            numOfInstructionsRun, instructionSet
+        )
+
+        if hasTerminatedSuccessfully:
+            break
 
     print(f"{accumulator=}")
     return 0
