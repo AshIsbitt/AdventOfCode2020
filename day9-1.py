86ea855c0951bc1220433dbb16b9cba0d78ab788
AshIsbitt <ashisbitt@icloud.com>
2021-11-19 15:02:16 +0000

    Day 9 pt1 structure

---
+++ day9-1.py
@@ -0,0 +1,12 @@
+# The first step of attacking the weakness in the XMAS data is to find the
+# first number in the list (after the preamble) which is not the sum of two of
+# the 25 numbers before it. What is the first number that does not have this
+# property?
+
+
+def main(filename: str) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main("SuppliedInputs/day9.txt"))
