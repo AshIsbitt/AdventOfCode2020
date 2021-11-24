f01a03ac2774824cd3b2088c2021b34e7f86e9fe
AshIsbitt <ashisbitt@icloud.com>
2021-11-24 13:42:48 +0000

    Part 2 preliminary code and fixed part 1 tests

--- day9-2.py
+++ day9-2.py
@@ -7,6 +7,7 @@
 # numbers?
 
 import pytest
+from typing import Union
 
 
 def validityChecker(currentVal: int, prevVals: list[int]) -> bool:
@@ -14,16 +15,14 @@ def validityChecker(currentVal: int, prevVals: list[int]) -> bool:
 
     for item in prevVals[-25:]:
         buf = currentVal - item
-        if buf in prevVals[-25:]:  # and buf != item:
+        if buf in prevVals[-25:] and buf != item:
             ret = True
             break
 
     return ret
 
 
-def locateAnomalousValue(returnedValues: list[str]) -> int:
-    dataValues = list(map(int, returnedValues))
-
+def locateAnomalousValue(dataValues: list[int]) -> int:
     previousNums = dataValues[:25]
     # print(f'{dataValues.index(135559)=}')
 
@@ -38,12 +37,35 @@ def locateAnomalousValue(returnedValues: list[str]) -> int:
     return item
 
 
+def encryptionWeakness(contiguousVals: list[int]) -> int:
+    return min(contiguousVals) + max(contiguousVals)
+
+
+def contiguousNumberFinder(value: int, dataValues: list[int]) -> list[int]:
+    for num in dataValues:
+        contiguousVals: list[int] = []
+
+        for prevNum in reversed(range(dataValues[0], dataValues[num])):
+            contiguousVals.append(prevNum)
+
+            if value == sum(contiguousVals):
+                return contiguousVals
+
+    return [0, 0]
+
+
 def main(filename: str) -> int:
     with open(filename) as inputFile:
         returnedValues = inputFile.readlines()
 
-    anomalousValue = locateAnomalousValue(returnedValues)
+    dataValues = list(map(int, returnedValues))
+
+    anomalousValue = locateAnomalousValue(dataValues)
     print(f"Part 1: {anomalousValue=}")
+
+    invalidNumberSet = contiguousNumberFinder(anomalousValue, dataValues)
+    anomalousSum = encryptionWeakness(invalidNumberSet)
+    print(f"Part 2: {anomalousSum}")
 
     return 0
 
@@ -64,6 +86,40 @@ def test_validityChecker(
     input_list: list[int], input_value: int, expected: bool
 ) -> None:
     assert validityChecker(input_value, input_list) == expected
+
+
+test_list_pt2 = [
+    35,
+    20,
+    15,
+    25,
+    47,
+    40,
+    62,
+    55,
+    65,
+    95,
+    102,
+    117,
+    150,
+    182,
+    127,
+    219,
+    299,
+    277,
+    309,
+    576,
+]
+
+
+@pytest.mark.parametrize(
+    ("input_value", "input_list", "output_list"),
+    ((127, test_list_pt2, [15, 25, 47, 40]),),
+)
+def test_contiguousNumberFinder(
+    input_value: int, input_list: list[int], output_list: list[int]
+) -> None:
+    assert contiguousNumberFinder(input_value, input_list) == output_list
 
 
 if __name__ == "__main__":
