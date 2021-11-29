c76997bf8445ca744d93ea84433238826ff36630
AshIsbitt <ashisbitt@icloud.com>
2021-11-29 21:17:46 +0000

    Day 10 first tests written

--- day10.py
+++ day10.py
@@ -5,6 +5,13 @@
 import pytest
 
 
+def joltageChainCalculator() -> tuple[int, int]:
+    count_ones: int = 0
+    count_threes: int = 0
+
+    return count_ones, count_threes
+
+
 def main(filename: str) -> int:
     with open(filename) as inputFile:
         adapterData = inputFile.readlines()
@@ -15,5 +22,53 @@ def main(filename: str) -> int:
 if __name__ == "__main__":
     raise SystemExit(main("SuppliedInputs/day10.txt"))
 
+################################################################################
 
-test_data == []
+test_data = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+]
+
+test_data_2 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+
+
+@pytest.mark.parametrize(
+    ("input_list", "expected"),
+    (
+        (test_data, (22, 10)),
+        (test_data_2, (7, 5)),
+    ),
+)
+def test_joltageChainCalculator(
+    input_list: list[int], expected: tuple[int, int]
+) -> None:
+    assert joltageChainCalculator(input_list) == expected
